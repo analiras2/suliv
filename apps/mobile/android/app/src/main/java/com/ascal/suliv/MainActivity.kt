df411f4b56cbe0de75030e1f217001d122a9db44
Analira Scalabrini <analira.scalabrini@gmail.com>
2026-04-24 10:39:33 -0400

    chore(mobile): align app identifier to com.ascal.suliv

--- apps/mobile/android/app/src/main/java/com/ascal/suliv/MainActivity.kt
+++ apps/mobile/android/app/src/main/java/com/ascal/suliv/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.suliv
+package com.ascal.suliv
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- apps/mobile/android/app/src/main/java/com/ascal/suliv/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/ascal/suliv/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.suliv
+package com.ascal.suliv
 
 import android.app.Application
 import android.content.res.Configuration
